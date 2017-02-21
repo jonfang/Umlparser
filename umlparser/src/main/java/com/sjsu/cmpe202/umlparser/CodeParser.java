3a603fe8d8f05bd2523bab8108ccb9a95dce1faf
jonfang <jonfang724@gmail.com>
2017-02-20 23:36:20 -0800

    2/20/2017 11:36 pm

--- umlparser/src/main/java/com/sjsu/cmpe202/umlparser/CodeParser.java
+++ umlparser/src/main/java/com/sjsu/cmpe202/umlparser/CodeParser.java
@@ -1,8 +1,12 @@
 package com.sjsu.cmpe202.umlparser;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 import com.github.javaparser.JavaParser;
 import com.github.javaparser.ast.CompilationUnit;
@@ -20,64 +24,138 @@ import com.github.javaparser.ast.nodeTypes.NodeWithVariables;
 
 public class CodeParser {
 
-	private static final String source_file = "src/test/test.java";
-	private StringBuilder yuml_string= new StringBuilder();
+	private static final String source_file = "src/test/uml-parser-test-1/";
+	private StringBuilder yuml_string; //stores the resulted string for diagram generator
+	private List<CompilationUnit> cu_list; //stores AST trees of all source codes
+	private List<String> class_list; //list that contains all the classes
+	private HashMap<String,List<String>> variable_list; //variable list correspond to mapped class
+	private HashMap<String, HashMap<String, Integer>> multi_map; //multiplicity map that stores class relations  
 	
 	public CodeParser(){
-		FileInputStream in = null;
-		try {
-			in = new FileInputStream(source_file);	//read input file
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		//initialize variables
+		 yuml_string= new StringBuilder();
+		 cu_list = new ArrayList();
+		 class_list = new ArrayList();
+		 variable_list = new HashMap();
+		 multi_map = new HashMap();
+		
+		//read sources files  
+		readSourceFiles(source_file,cu_list);
+		//store classes
+		for(CompilationUnit cu:cu_list){
+			getClass(cu, class_list);
 		}
-        // parse the file
-        CompilationUnit cu = JavaParser.parse(in);
-        System.out.println(cu.toString());
-        process(cu);
-        // prints the resulting compilation unit to default system output
+		for(String class_name:class_list){
+			variable_list.put(class_name, new ArrayList()); //Initialize variable list for each class
+		}
+		for(int i=0;i<cu_list.size();i++){
+			process(cu_list.get(i), class_list.get(i), variable_list);
+		}
+		/*
+		for(String c:class_list){
+			System.out.println(c);
+		}
+		*/
+        //process(cu);
+		construct();
+       
         
 	}
 	
-	 public void process(Node node){
+	public void getClass(Node node, List<String> class_list){ //later modify this to interface
+		if (node instanceof TypeDeclaration) {
+		      // do something with this type declaration
+			 ClassOrInterfaceDeclaration class_name = (ClassOrInterfaceDeclaration) node;
+			 class_list.add(class_name.getName().toString());
+		}
+		for(Node child:node.getChildNodes()){
+			getClass(child, class_list);
+		}
+	}
+	
+
+	
+	public void process(Node node, String class_name, HashMap<String,List<String>> variable_list){
 		 if (node instanceof TypeDeclaration) {
 		      // do something with this type declaration
 			 ClassOrInterfaceDeclaration dec = (ClassOrInterfaceDeclaration) node;
 			 //get class name
-			 yuml_string.append("[" + dec.getName()+"|");
 		 } else if (node instanceof MethodDeclaration) {
 		      // do something with this method declaration
 			   System.out.println("Method: " + node);
 		   } else if (node instanceof FieldDeclaration) {
 		      // do something with this field declaration
 			   FieldDeclaration field = (FieldDeclaration)node;
+			   List<String> var_list = variable_list.get(class_name);
+			   StringBuilder var = new StringBuilder();
 			   //set visibility
 			   if(field.isPrivate()){
-				   yuml_string.append("-");
+				   var.append("-");
 			   }
 			   else if(field.isProtected()){
-				   yuml_string.append("+");
+				   var.append("+");
 			   }
 			   //get type
 			   String type = field.getCommonType().toString();
 			   if(type.contains("[]")){ //array of elements
 				   type = type.replace("[]", "(*)");
 			   }
-			   yuml_string.append(type + ":");
+			   var.append(type + ":");
 			   //get variable
-			   for(VariableDeclarator var:field.getVariables()){
+			   for(VariableDeclarator v:field.getVariables()){
 				   //*do check later here for multiple variables
-				   yuml_string.append(var+";");
+				   var.append(v+";");
 			   }
+			   var_list.add(var.toString());
 		   }
 		    // Do something with the node
 		    for (Node child : node.getChildNodes()){
-		    	process(child);
-		    }
-		    if(node instanceof TypeDeclaration){ //finish
-		    	yuml_string.append("]");
+		    	process(child, class_name, variable_list);
 		    }
 	}
+	 
+	 private void readSourceFiles(String path, List<CompilationUnit> cu_list){
+		 File folder = new File(path);
+		 FileInputStream instream = null;
+		 for(File file:folder.listFiles()){
+			 if(file.getName().endsWith("java") &&file.isFile()){
+					try {
+						instream = new FileInputStream(file);
+					} catch (FileNotFoundException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					CompilationUnit cu = JavaParser.parse(instream);
+					cu_list.add(cu);
+					try {
+						instream.close();
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}	
+			}
+		 } 
+	 }
+	 
+	 public void derive_multiplicity(){ //get multiplicity
+		 
+	 }
+	 
+	 public void construct(){
+		 for(String class_name:class_list){
+			 System.out.println(class_name);
+			 System.out.println(variable_list.get(class_name));
+		 }
+		 /*
+		 for(String class_name:class_list){
+			 yuml_string.append("[" + class_name+"|");
+			 for(String var: variable_list.get(class_name)){
+				 yuml_string.append(var);
+			 }
+			 yuml_string.append("]"+",");
+		 }
+		 */
+	 }
 	 
 	 public String generateString(){
 		 return yuml_string.toString();
