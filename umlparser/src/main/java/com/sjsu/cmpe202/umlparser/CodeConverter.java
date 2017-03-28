6c3ff6302de3bf9a2b2cf830e8c491cb5561da6c
jonfang <jfang@tibco.com>
2017-03-28 16:38:57 -0700

    3/28/2017 4:38 update: implment codeConverter

--- umlparser/src/main/java/com/sjsu/cmpe202/umlparser/CodeConverter.java
+++ umlparser/src/main/java/com/sjsu/cmpe202/umlparser/CodeConverter.java
@@ -4,6 +4,7 @@ public class CodeConverter {
 	
 	public String generateYUMLString(){
 		//generate string usable by YUML for UML Class Diagram
+		//generate string usable by YUML for UML Class Diagram
 		return "[A|-x:int;-y:int(*)]1-0..*[B],[A]-1[C],[A]-*[D]";
 	}
 }
